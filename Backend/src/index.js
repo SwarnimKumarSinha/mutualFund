6871d54bfa6edf8b0db334f14923415259146c4d
Swarnim Kumar Sinha <127919354+SwarnimKumarSinha@users.noreply.github.com>
2023-03-15 21:35:31 +0530

    Add files via upload

---
+++ Backend/src/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connect = require("./config/db");
+const app = express();
+const User = require("./features/user/user.route");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/users", User);
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("This is Home Route of Shilpi page Backend");
+});
+
+app.listen(PORT, async () => {
+  await connect();
+});
+
